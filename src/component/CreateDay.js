68822949c9171a9a21bb34ca71e6df5e0dd98de5
jaeilnet <sgsgself@naver.com>
2021-08-17 23:58:27 +0900

    유투브 코딩악마님 리액트 강의(단어추가)

---
+++ src/component/CreateDay.js
@@ -0,0 +1,5 @@
+export default function CreateDay () {
+  return(
+    <div> 여긴 day</div>
+  )
+}
---
+++ src/component/DayList.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import useFetch from "../hooks/useFetch"
+
+export default function DayList() {
+
+  const days = useFetch('http://localhost:3001/days')
+  
+  return (
+    <div>
+      <ul className="list_day">
+        {days.map(day => (
+          <li key={day.id}>
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
